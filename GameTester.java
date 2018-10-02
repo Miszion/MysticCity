b2c8b6700bea4539eeeca27c9aca281658609c3a
Miszion <missionmarcus@gmail.com>
2018-10-02 11:48:24 -0500

    added gameTester file

---
+++ GameTester.java
@@ -0,0 +1,63 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+// Mission Marcus
+// mmarcu6 PROJ 2
+
+public class GameTester {
+	
+	
+	
+	public static void main(String [] args) throws IOException
+	{
+		System.out.println("Mission Marcus"); // my name is here.
+		System.out.println("NETID: mmarcu6"); // netid
+		Scanner scan = new Scanner(System.in);
+		String input = "";
+		Scanner fileScanner; // file scanner
+		
+		boolean worked = false;
+		
+		System.out.println("Please enter in a file name");
+		
+		while (worked == false  && !input.toLowerCase().equals("quit") && !input.toLowerCase().equals("q") && !input.toLowerCase().equals("exit"))
+		{
+		try
+		{
+		input = scan.nextLine();
+		File fin = new File(input);
+		
+		fileScanner = new Scanner(new FileInputStream(fin), "UTF-8");
+		
+		Game newGame = new Game(fileScanner);
+		worked = true;
+		newGame.play();
+		scan.close();
+		fileScanner.close();
+		}
+		catch(FileNotFoundException ex)
+		{
+			if (input.toLowerCase().equals("quit") || input.toLowerCase().equals("q") || input.toLowerCase().equals("exit"))
+			{
+			worked = true;
+			}
+			else
+			{
+				worked = false;
+				System.out.println("File was not found, Please enter a new filename");
+			}
+			
+		}
+		}
+		
+		
+		
+
+		
+	}
+	
+}
